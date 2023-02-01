4bc42c23e69de78c28889b18cf760b50d1f1b384
working1153 <116803406+working1153@users.noreply.github.com>
2023-02-01 20:12:58 +0800

    secondchange
    second

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,7 +52,6 @@ const registerUser = asyncHandler(async (req, res) => {
 //@desc Login a new user
 //@route /api/users/login
 //@access Public
-
 const loginUser = asyncHandler(async (req, res) => {
   const {email, password} = req.body
 
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-
+//auth user
 const protect = asyncHandler(async (req, res, next) => {
   let token
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
